fabd684e702de1483635493382527f5eadaf0176
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-22 05:38:18 +0400

    Rubocop autofixes

--- app/controllers/tramway/export/application_controller.rb
+++ app/controllers/tramway/export/application_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-class Tramway::Export::ApplicationController < Tramway::ApplicationController
-  before_action :authenticate_admin!
+module Tramway
+  module Export
+    class ApplicationController < Tramway::ApplicationController
+      before_action :authenticate_admin!
+    end
+  end
 end
--- app/controllers/tramway/has_and_belongs_to_many_records_controller.rb
+++ app/controllers/tramway/has_and_belongs_to_many_records_controller.rb
@@ -1,29 +1,25 @@
 # frozen_string_literal: true
 
-class Tramway::HasAndBelongsToManyRecordsController < ::Tramway::ApplicationController
-  def create
-    base_object = params[:model_class].constantize.find params[:object_id]
-    form_class = params[:form].constantize
-    record_form = form_class.new base_object
-    sending_params = if params[params[:model_class].underscore].present?
-                       params[params[:model_class].underscore]
-                     else
-                       params[form_class.associated_as]
-                     end
-    if record_form.submit sending_params
-      redirect_to params[:redirect].present? ? params[:redirect] : record_path(base_object, model: base_object.class)
-    else
-      redirect_to params[:redirect].present? ? params[:redirect] : record_path(base_object, model: base_object.class)
+module Tramway
+  class HasAndBelongsToManyRecordsController < ::Tramway::ApplicationController
+    def create
+      base_object = params[:model_class].constantize.find params[:object_id]
+      form_class = params[:form].constantize
+      record_form = form_class.new base_object
+      sending_params = if params[params[:model_class].underscore].present?
+                         params[params[:model_class].underscore]
+                       else
+                         params[form_class.associated_as]
+                       end
+      path = params[:redirect].present? ? params[:redirect] : record_path(base_object, model: base_object.class)
+      redirect_to path
     end
-  end
 
-  def destroy
-    base_object = params[:model_class].constantize.find params[:object_id]
-    record_form = params[:form].constantize.new base_object
-    if record_form.submit params[:id]
-      redirect_to params[:redirect].present? ? params[:redirect] : record_path(base_object, model: base_object.class)
-    else
-      redirect_to params[:redirect].present? ? params[:redirect] : record_path(base_object, model: base_object.class)
+    def destroy
+      base_object = params[:model_class].constantize.find params[:object_id]
+      record_form = params[:form].constantize.new base_object
+      redirect_to params[:redirect].present? ? params[:redirect] : record_path(base_object,
+                                                                               model: base_object.class)
     end
   end
 end
