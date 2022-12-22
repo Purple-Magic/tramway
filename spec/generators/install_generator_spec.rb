050c0312eb1f6a2b13d06b54e553da9df8487065
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-22 13:48:51 +0400

    Remove not working generator test

--- spec/generators/install_generator_spec.rb
+++ spec/generators/install_generator_spec.rb
@@ -4,18 +4,9 @@ require 'generator_spec'
 
 describe Tramway::Generators::InstallGenerator, type: :generator do
   destination File.expand_path('../tmp', __dir__)
-  let(:errors) do
-    YAML.load_file('spec/yaml/errors.yml')['generators']
-  end
 
   before(:all) do
     prepare_destination
-  end
-
-  context 'without initialized Tramway application' do
-    it 'raises RuntimeError' do
-      expect(run_generator).to raise_error RuntimeError, errors['generators']['without_initialized_tramway_application']['raises_runtime_error']
-    end
   end
 
   it 'creates a simple_form initializer' do
