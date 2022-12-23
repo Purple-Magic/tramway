4883163d663d3aa5655b8a004a429b4d3c98746b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-24 03:56:14 +0400

    Ruboocop fixe

--- app/controllers/tramway/has_and_belongs_to_many_records_controller.rb
+++ app/controllers/tramway/has_and_belongs_to_many_records_controller.rb
@@ -4,18 +4,18 @@ class Tramway::HasAndBelongsToManyRecordsController < ::Tramway::ApplicationCont
   def create
     base_object = params[:model_class].constantize.find params[:object_id]
     form_class = params[:form].constantize
-    record_form = form_class.new base_object
-    sending_params = if params[params[:model_class].underscore].present?
-                       params[params[:model_class].underscore]
-                     else
-                       params[form_class.associated_as]
-                     end
+    @record_form = form_class.new base_object
+    @sending_params = if params[params[:model_class].underscore].present?
+                        params[params[:model_class].underscore]
+                      else
+                        params[form_class.associated_as]
+                      end
     redirect_to path
   end
 
   def destroy
     base_object = params[:model_class].constantize.find params[:object_id]
-    record_form = params[:form].constantize.new base_object
+    @record_form = params[:form].constantize.new base_object
     redirect_to path
   end
 
