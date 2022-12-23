9ca1632be3557a53d19930e69f416402cec1c0f8
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-24 03:28:48 +0400

    Rubocop fixes

--- app/helpers/tramway/frontend_helper.rb
+++ app/helpers/tramway/frontend_helper.rb
@@ -2,13 +2,7 @@
 
 module Tramway::FrontendHelper
   def react_params(form, action, method)
-    url = case action
-          when :create
-            Tramway::Engine.routes.url_helpers.records_path(model: form.model.class)
-          else
-            Tramway::Engine.routes.url_helpers.record_path(form.model.id, model: form.model.class)
-          end
-    form.properties.each_with_object({ action: url, method: method,
+    form.properties.each_with_object({ action: react_params_url(form, action), method: method,
                                        authenticity_token: form_authenticity_token }) do |property, hash|
       case property[1]
       when :association
@@ -19,5 +13,14 @@ module Tramway::FrontendHelper
         )
       end
     end.merge model: form.model.attributes
+  end
+
+  def react_params_url(form, action)
+    case action
+    when :create
+      Tramway::Engine.routes.url_helpers.records_path(model: form.model.class)
+    else
+      Tramway::Engine.routes.url_helpers.record_path(form.model.id, model: form.model.class)
+    end
   end
 end
