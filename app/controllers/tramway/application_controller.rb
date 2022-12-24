ad6d42194ab945bd9fa9c7ea405c48e2b1d3a9dd
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-24 04:08:22 +0400

    Rubocop fixes

--- app/controllers/tramway/application_controller.rb
+++ app/controllers/tramway/application_controller.rb
@@ -10,6 +10,7 @@ load "#{Tramway.root}/app/controllers/concerns/auth_management.rb"
 class Tramway::ApplicationController < ActionController::Base
   include Tramway::ClassNameHelpers
   include Tramway::AuthManagement
+  include Tramway::Filtering
   include Tramway::RecordRoutesHelper
 
   before_action :application
@@ -138,47 +139,7 @@ class Tramway::ApplicationController < ActionController::Base
     (defined?(current_user) && current_user.try(:model)) || (defined?(current_user) && current_user.model)
   end
 
-  def list_filtering(records)
-    params[:list_filters]&.each do |filter, _value|
-      case decorator_class.list_filters[filter.to_sym][:type]
-      when :select
-        records = list_filtering_select records, filter
-      when :dates
-        records = list_filtering_dates records, filter
-      end
-    end
-
-    records
-  end
-
-  def list_filtering_select(records, filter)
-    value.present? ? decorator_class.list_filters[filter.to_sym][:query].call(records, value) : records
-  end
-
-  def list_filtering_dates(records, filter)
-    begin_date = date_filter :begin, filter
-    end_date = date_filter :end, filter
-    if begin_date.present? && end_date.present? && value.present?
-      decorator_class.list_filters[filter.to_sym][:query].call(records, begin_date, end_date)
-    else
-      records
-    end
-  end
-
-  def date_filter(type, filter)
-    params[:list_filters][filter.to_sym]["#{type}_date".to_sym]
-  end
-
   def current_user_role_scope
     "#{current_user.role}_scope"
-  end
-
-  def filtering(records)
-    if params[:filter].present?
-      params[:filter] = JSON.parse params[:filter] if params[:filter].is_a? String
-      records.ransack(params[:filter]).result(distinct: true)
-    else
-      records
-    end
   end
 end
