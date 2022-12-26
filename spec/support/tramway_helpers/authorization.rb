15f464dbbc58231fc04b37b0b5b89581a566140f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-12-27 00:56:16 +0400

    Rubocop fixes

--- spec/support/tramway_helpers/authorization.rb
+++ spec/support/tramway_helpers/authorization.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
-module TramwayHelpers::Authorization
-  def pass_authorization(user)
-    visit home_page
-    fill_in 'Email', with: user[:email]
-    fill_in 'Password', with: '123'
-    click_on 'Sign In', class: 'btn-success'
+# rubocop:disable Style/ClassAndModuleChildren
+module TramwayHelpers
+  module Authorization
+    def pass_authorization(user)
+      visit home_page
+      fill_in 'Email', with: user[:email]
+      fill_in 'Password', with: '123'
+      click_on 'Sign In', class: 'btn-success'
+    end
   end
 end
+# rubocop:enable Style/ClassAndModuleChildren
 
 RSpec.configure do |config|
   config.include TramwayHelpers::Authorization
