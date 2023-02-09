adad92696dc181cb3feabaf7f45668c8282f741e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-02-10 03:55:33 +0400

    Separate commands in docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,9 @@ set -e
 bundle install --binstubs="$BUNDLE_BIN" --gemfile=./Gemfile
 
 cd spec/dummy
-bundle exec rails db:create db:migrate
+bundle exec rails db:create
 bundle exec rails g tramway:install
+bundle exec rails db:migrate
 cd ../..
 
 exec "$@"
