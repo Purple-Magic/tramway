540ed3826c5f3f4787e4b80ac524374db961cc44
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-02-10 03:51:16 +0400

    Change order commands in docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 bundle install --binstubs="$BUNDLE_BIN" --gemfile=./Gemfile
 
 cd spec/dummy
-bundle exec rails g tramway:install
 bundle exec rails db:create db:migrate
+bundle exec rails g tramway:install
 cd ../..
 
 exec "$@"
