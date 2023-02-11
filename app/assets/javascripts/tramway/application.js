35b7b47615667addc661c437c7bc7aa9223fd646
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-02-12 03:39:08 +0400

    Minor fixes

--- app/assets/javascripts/tramway/application.js
+++ app/assets/javascripts/tramway/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery3
+//= require popper
 //= require bootstrap-datepicker-1.8.0
 //= require bootstrap-datepicker-1.8.0.ru.min
 //= require bootstrap-sprockets
