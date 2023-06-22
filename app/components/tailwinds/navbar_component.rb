442ed46ef3835a637989ee0f2fd989ad2c29b921
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-06-23 00:15:22 +0400

    Navbar color options (#24)
    * Add Navbar color options
    
    * Add tests
    
    * Add more tests
    
    * Update README.md

--- app/components/tailwinds/navbar_component.rb
+++ app/components/tailwinds/navbar_component.rb
@@ -8,6 +8,29 @@ module Tailwinds
       @title = options[:title]
       @left_items = options[:left_items]
       @right_items = options[:right_items]
+      @color = background(options)
+    end
+
+    private
+
+    CSS_COLORS = %i[aqua black blue fuchsia gray green lime maroon navy olive orange purple red
+                    silver teal white yellow].freeze
+    MIN_INTENSITY = 100
+    MAX_INTENSITY = 950
+
+    def background(options)
+      color = options.dig(:background, :color) || :purple
+      intensity = options.dig(:background, :intensity) || 700
+
+      unless (MIN_INTENSITY..MAX_INTENSITY).cover?(intensity)
+        raise "Navigation Background Color intensity should be between #{MIN_INTENSITY} and #{MAX_INTENSITY}"
+      end
+
+      if color.in? CSS_COLORS
+        "#{color}-#{intensity}"
+      else
+        "[#{color}]"
+      end
     end
   end
 end
--- spec/components/tailwinds/navbar_component_spec.rb
+++ spec/components/tailwinds/navbar_component_spec.rb
@@ -3,30 +3,82 @@
 require 'rails_helper'
 
 describe Tailwinds::NavbarComponent, type: :component do
-  it 'renders title' do
-    render_inline(described_class.new(title: 'Purple Magic'))
+  context 'with title checks' do
+    it 'renders title' do
+      render_inline(described_class.new(title: 'Purple Magic'))
 
-    expect(page).to have_text 'Purple Magic'
+      expect(page).to have_text 'Purple Magic'
+    end
   end
 
-  it 'renders left items' do
-    render_inline(described_class.new(left_items: ["<a href='/test'>Test</a>"]))
+  context 'with items checks' do
+    it 'renders left items' do
+      render_inline(described_class.new(left_items: ["<a href='/test'>Test</a>"]))
 
-    expect(page).to have_css 'nav .flex ul.flex.items-center.space-x-4'
+      expect(page).to have_css 'nav .flex ul.flex.items-center.space-x-4'
+    end
+
+    it 'renders right items' do
+      render_inline(described_class.new(right_items: ["<a href='/test'>Test</a>"]))
+
+      expect(page).to have_css 'nav ul.flex.items-center.space-x-4'
+    end
+
+    it 'renders left and right items' do
+      links = ["<a href='/test'>Test</a>"]
+
+      render_inline(described_class.new(right_items: links, left_items: links))
+
+      expect(page).to have_css 'nav .flex ul.flex.items-center.space-x-4'
+      expect(page).to have_css 'nav ul.flex.items-center.space-x-4'
+    end
   end
 
-  it 'renders right items' do
-    render_inline(described_class.new(right_items: ["<a href='/test'>Test</a>"]))
+  context 'with background checks' do
+    it 'renders navbar with default colors' do
+      render_inline(described_class.new)
 
-    expect(page).to have_css 'nav ul.flex.items-center.space-x-4'
-  end
+      expect(page).to have_css 'nav.bg-purple-700'
+    end
 
-  it 'renders left and right items' do
-    links = ["<a href='/test'>Test</a>"]
+    it 'renders navbar with named color and default intensity' do
+      color = Tailwinds::NavbarComponent::CSS_COLORS.sample
 
-    render_inline(described_class.new(right_items: links, left_items: links))
+      render_inline(described_class.new(background: { color: }))
 
-    expect(page).to have_css 'nav .flex ul.flex.items-center.space-x-4'
-    expect(page).to have_css 'nav ul.flex.items-center.space-x-4'
+      expect(page).to have_css "nav.bg-#{color}-700"
+    end
+
+    it 'renders navbar with named color and intensity' do
+      color = Tailwinds::NavbarComponent::CSS_COLORS.sample
+      intensity = rand(Tailwinds::NavbarComponent::MIN_INTENSITY..Tailwinds::NavbarComponent::MAX_INTENSITY)
+
+      render_inline(described_class.new(background: { color:, intensity: }))
+
+      expect(page).to have_css "nav.bg-#{color}-#{intensity}"
+    end
+
+    it 'renders navbar with hex color' do
+      color = "##{SecureRandom.hex(3)}"
+
+      render_inline(described_class.new(background: { color: }))
+
+      # NOTE: we need it because this line returns error, `[` is not expected in selectors
+      # expect(page).to have_css "nav.bg-[#{color}]"
+
+      navbar_class = page.native.children[1].children[0].children[0].attribute_nodes[0].value
+      expect(navbar_class).to include("bg-[#{color}]")
+    end
+
+    it 'raises error with not suitable intensity' do
+      color = Tailwinds::NavbarComponent::CSS_COLORS.sample
+      intensity = 951
+
+      expect do
+        render_inline(described_class.new(background: { color:, intensity: }))
+      end.to raise_error(
+        'Navigation Background Color intensity should be between 100 and 950'
+      )
+    end
   end
 end
