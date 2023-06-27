6150eae3d4764a7c11313c5c228b0feb5a80a037
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-06-27 18:05:25 +0400

    Fix test

--- lib/tramway/helpers/decorate_helper.rb
+++ lib/tramway/helpers/decorate_helper.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+require 'tramway/decorators/class_helper'
+
 module Tramway
   module Helpers
     # Provides methods into Rails ActionController
     #
     module DecorateHelper
       def tramway_decorate(object_or_array, decorator: nil)
-        decorator_class(object_or_array, decorator).decorate object_or_array, self
+        Tramway::Decorators::ClassHelper.decorator_class(object_or_array, decorator).decorate object_or_array, self
       end
     end
   end
