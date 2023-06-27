b50abfbd82988187cf1a3b7bcdcf62615d9810cb
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-06-27 13:48:51 +0400

    Fix with ChatGPT

--- app/components/tailwinds/navbar_component.rb
+++ app/components/tailwinds/navbar_component.rb
@@ -1,26 +1,19 @@
 # frozen_string_literal: true
 
 module Tailwinds
-  # Navbar class main class
+  # Background helper module
   #
-  class NavbarComponent < TailwindComponent
-    def initialize(**options)
-      @title = options[:title]
-      @left_items = options[:left_items]
-      @right_items = options[:right_items]
-      @color = background(options)
-    end
-
-    private
-
+  module BackgroundHelper
     CSS_COLORS = %i[aqua black blue fuchsia gray green lime maroon navy olive orange purple red
                     silver teal white yellow].freeze
     MIN_INTENSITY = 100
     MAX_INTENSITY = 950
+    DEFAULT_BACKGROUND_COLOR = :purple
+    DEFAULT_BACKGROUND_INTENSITY = 700
 
-    def background(options)
-      color = options.dig(:background, :color) || :purple
-      intensity = options.dig(:background, :intensity) || 700
+    def self.background(options)
+      color = options.dig(:background, :color) || DEFAULT_BACKGROUND_COLOR
+      intensity = options.dig(:background, :intensity) || DEFAULT_BACKGROUND_INTENSITY
 
       unless (MIN_INTENSITY..MAX_INTENSITY).cover?(intensity)
         raise "Navigation Background Color intensity should be between #{MIN_INTENSITY} and #{MAX_INTENSITY}"
@@ -31,6 +24,19 @@ module Tailwinds
       else
         "[#{color}]"
       end
+    end
+  end
+
+  # Navbar class main class
+  #
+  class NavbarComponent < TailwindComponent
+    include Tailwinds::BackgroundHelper
+
+    def initialize(**options)
+      @title = options[:title]
+      @left_items = options[:left_items]
+      @right_items = options[:right_items]
+      @color = BackgroundHelper.background(options)
     end
   end
 end
