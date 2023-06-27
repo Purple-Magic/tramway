f0a4407a744c0eebc7fcef34d5c6afa497114db7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-06-27 14:27:35 +0400

    Reek fixe

--- lib/tramway/navbar.rb
+++ lib/tramway/navbar.rb
@@ -1,48 +1,68 @@
 # frozen_string_literal: true
 
 module Tramway
-  # Navbar class for Tramway navbars
-  #
+  # Navbar object provides left and right elements
   class Navbar
     attr_reader :items
 
     def initialize(context)
       @items = {}
       @context = context
+      @filling = nil
     end
 
     def left
       return unless block_given?
 
       @items[:left] = []
-
-      @filling = :left
-
+      filling_side(:left)
       yield self
+      reset_filling
     end
 
     def right
       return unless block_given?
 
       @items[:right] = []
-
-      @filling = :right
-
+      filling_side(:right)
       yield self
+      reset_filling
     end
 
     def item(text_or_url, url = nil, **options, &block)
-      raise 'You can not provide argument and code block in the same time' if url.present? && block_given?
+      raise 'You cannot provide an argument and a code block at the same time' if provided_url_and_block?(url, &block)
 
       rendered_item = if url.present?
-                        options.merge! href: url
-                        @context.render(Tailwinds::Nav::ItemComponent.new(**options)) { text_or_url }
+                        render_ignoring_block(text_or_url, url, options)
                       else
-                        options.merge! href: text_or_url
-                        @context.render(Tailwinds::Nav::ItemComponent.new(**options), &block)
+                        render_using_block(text_or_url, options, &block)
                       end
 
       @items[@filling] << rendered_item
+    end
+
+    private
+
+    def provided_url_and_block?(url)
+      url.present? && block_given?
+    end
+
+    def filling_side(side)
+      @filling = side
+    end
+
+    def reset_filling
+      @filling = nil
+    end
+
+    def render_ignoring_block(text_or_url, url, options)
+      options.merge!(href: url)
+      @context.render(Tailwinds::Nav::ItemComponent.new(**options)) { text_or_url }
+    end
+
+    def render_using_block(text_or_url, options, &block)
+      options.merge!(href: text_or_url)
+      @context.render(Tailwinds::Nav::ItemComponent.new(**options), &block)
     end
   end
 end
