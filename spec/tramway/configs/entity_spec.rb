c4dbfe485564c505ab1f2a99824e6512e00247d2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-08-15 11:58:12 +0400

    Refactor tests

--- spec/tramway/configs/entity_spec.rb
+++ spec/tramway/configs/entity_spec.rb
@@ -22,33 +22,15 @@ shared_examples 'Tramway Config Entity human_name' do |name|
   end
 end
 
-shared_examples 'Tramway Config Entity routes' do |name, route|
-  subject { described_class.new(name:, route:) }
-
+shared_examples 'Tramway Config Entity routes' do
   describe '#routes' do
     it 'returns an OpenStruct object with index route' do
       expect(subject.routes).to be_an(OpenStruct)
       expect(subject.routes).to respond_to(:index)
     end
 
-    let(:expected_route) do
-      helper = if route.present?
-                 if route[:namespace].present? && route[:route_method].nil?
-                   "#{route[:namespace]}_#{name.to_s.pluralize.parameterize.underscore}_path"
-                 elsif route[:namespace].present? && route[:route_method].present?
-                   "#{route[:namespace]}_#{route[:route_method]}_path"
-                 elsif route[:namespace].nil? && route[:route_method].present?
-                   "#{route[:route_method]}_path"
-                 end
-               else
-                 "#{name.to_s.pluralize.parameterize.underscore}_path"
-               end
-
-      Rails.application.routes.url_helpers.public_send helper
-    end
-
     it 'sets the correct index route' do
-      expect(subject.routes.index).to eq(expected_route)
+      expect(subject.routes.index).to eq(Rails.application.routes.url_helpers.public_send(helper))
     end
   end
 end
@@ -59,26 +41,45 @@ describe Tramway::Configs::Entity do
       entity = :user
 
       include_examples 'Tramway Config Entity human_name', entity
-      include_examples 'Tramway Config Entity routes', entity
     end
 
     context 'with entity with namespaces' do
       entity = 'episodes/part'
 
       include_examples 'Tramway Config Entity human_name', entity
-      include_examples 'Tramway Config Entity routes', entity
     end
   end
 
   context 'with entity name and route contains namespace only' do
-    include_examples 'Tramway Config Entity routes', :user, { namespace: :admin }
+    let(:name) { :user }
+    let(:route) { { namespace: :admin } }
+
+    subject { described_class.new(name:, route:) }
+
+    let(:helper) { "#{route[:namespace]}_#{name.to_s.pluralize}_path" }
+
+    include_examples 'Tramway Config Entity routes'
   end
 
   context 'with entity name and route contains namespace and route_method' do
-    include_examples 'Tramway Config Entity routes', :user, { namespace: :admin, route_method: :clients }
+    let(:name) { :user }
+    let(:route) { { namespace: :admin, route_method: :clients } }
+
+    subject { described_class.new(name:, route:) }
+
+    let(:helper) { "#{route[:namespace]}_#{route[:route_method]}_path" }
+
+    include_examples 'Tramway Config Entity routes'
   end
 
   context 'with entity name and route contains route_method only' do
-    include_examples 'Tramway Config Entity routes', :user, { route_method: :clients }
+    let(:name) { :user }
+    let(:route) { { route_method: :clients } }
+
+    subject { described_class.new(name:, route:) }
+
+    let(:helper) { "#{route[:route_method]}_path" }
+
+    include_examples 'Tramway Config Entity routes'
   end
 end
