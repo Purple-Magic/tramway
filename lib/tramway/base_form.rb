788da9f16edf7eaa7183d738014bb19a60cdac9b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-08-31 03:29:20 +0400

    Tramway forms are ready

--- lib/tramway/base_form.rb
+++ lib/tramway/base_form.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-require 'dry-initializer'
-
 module Tramway
   # Provides form object for Tramway
   #
   class BaseForm
     attr_reader :object
 
-    [ :model_name, :to_key, :to_model, :errors, :attributes ].each do |method_name|
+    %i[model_name to_key to_model errors attributes].each do |method_name|
       delegate method_name, to: :object
     end
 
@@ -17,7 +15,7 @@ module Tramway
     end
 
     class << self
-      def property(attribute, proc_obj = nil)
+      def property(attribute, _proc_obj = nil)
         @properties ||= []
         @properties << attribute
 
@@ -38,18 +36,28 @@ module Tramway
     def submit(params)
       __submit params
 
-      object.save
+      object.save.tap do
+        object.reload
+      end
     end
 
     def submit!(params)
       __submit params
 
-      object.save!
+      object.save!.tap do
+        object.reload
+      end
+    end
+
+    def respond_to_missing?(method_name)
+      return true if method_name.to_s.end_with?('=')
+
+      super
     end
 
     def method_missing(method_name, *args)
       if method_name.to_s.end_with?('=') && args.count == 1
-        object.public_send method_name, args.first
+        object.public_send(method_name, args.first)
       else
         super
       end
--- lib/tramway/forms/class_helper.rb
+++ lib/tramway/forms/class_helper.rb
@@ -7,14 +7,14 @@ module Tramway
       module_function
 
       def form_class(object, form, namespace)
+        object_class = object.class
+
         if form.present?
           form
+        elsif namespace.present?
+          "#{namespace.to_s.camelize}::#{object_class}Form".constantize
         else
-          if namespace.present?
-            "#{namespace.to_s.camelize}::#{object.class}Form".constantize
-          else
-            "#{object.class}Form".constantize
-          end
+          "#{object_class}Form".constantize
         end
       end
     end
