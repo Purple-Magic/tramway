f8aa3b3dfa57f1a1910ee791352c7615dbff2fbc
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-09-01 01:46:44 +0400

    Tests and fixes for Tramway Form

---
+++ spec/helpers/form_helper_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Tramway::Helpers::FormHelper, type: :helper do
+  let(:dummy_class) do
+    Class.new do
+      include Tramway::Helpers::FormHelper
+    end
+  end
+
+  let(:dummy_instance) { dummy_class.new }
+
+  describe '#tramway_form' do
+    it 'returns a form object' do
+      object = double('Object')
+      form_class = double('FormClass')
+      form_object = double('FormObject')
+
+      expect(Tramway::Forms::ClassHelper).to receive(:form_class).with(object, nil, nil).and_return(form_class)
+
+      expect(form_class).to receive(:new).with(object).and_return(form_object)
+
+      expect(dummy_instance.tramway_form(object)).to eq(form_object)
+    end
+
+    it 'passes form and namespace options to ClassHelper' do
+      object = double('Object')
+      form = double('Form')
+      namespace = double('Namespace')
+      form_class = double('FormClass')
+      form_object = double('FormObject')
+
+      expect(Tramway::Forms::ClassHelper).to receive(:form_class).with(object, form, namespace).and_return(form_class)
+      expect(form_class).to receive(:new).with(object).and_return(form_object)
+
+      expect(dummy_instance.tramway_form(object, form:, namespace:)).to eq(form_object)
+    end
+  end
+end
