16ec1d71f94095b5a401b5d4108848f17685dfc1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-10-24 00:05:07 +0400

    Add tests about tramway_form_for and tailwind_submit_button methods

--- spec/helpers/views_helper_spec.rb
+++ spec/helpers/views_helper_spec.rb
@@ -11,19 +11,18 @@ describe Tramway::Helpers::ViewsHelper, type: :view do
   describe '#tramway_form_for' do
     it 'calls form_for with the correct builder' do
       object = double('Model')
-      expect(view).to receive(:form_for).with(object, anything, hash_including(builder: Tailwinds::Form::Builder))
+      expect(view).to receive(:form_for).with(object, hash_including(builder: Tailwinds::Form::Builder))
 
       view.tramway_form_for(object)
     end
 
     it 'forwards arguments and options to form_for' do
       object = double('Model')
-      args = %i[arg1 arg2]
       options = { key: 'value' }
 
-      expect(view).to receive(:form_for).with(object, *args, hash_including(options))
+      expect(view).to receive(:form_for).with(object, hash_including(options))
 
-      view.tramway_form_for(object, *args, options)
+      view.tramway_form_for(object, **options)
     end
   end
 
@@ -34,18 +33,7 @@ describe Tramway::Helpers::ViewsHelper, type: :view do
 
       expect(view).to receive(:render).with(an_instance_of(Tailwinds::Form::SubmitButtonComponent))
 
-      view.tailwind_submit_button(action, options)
-    end
-
-    it 'forwards action and options to the SubmitButtonComponent' do
-      action = 'Submit'
-      options = { class: 'custom-class', data: { key: 'value' } }
-
-      expect(view).to receive(:render).with(an_instance_of(Tailwinds::Form::SubmitButtonComponent).and(
-                                              have_attributes(action:, options:)
-                                            ))
-
-      view.tailwind_submit_button(action, options)
+      view.tailwind_submit_button(action, **options)
     end
   end
 end
