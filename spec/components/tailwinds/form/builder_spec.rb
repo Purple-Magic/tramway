537834a92447af1b4448f9efaf1e39ca94f4f436
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-10-23 23:50:09 +0400

    Add #password_field and #file_field

--- spec/components/tailwinds/form/builder_spec.rb
+++ spec/components/tailwinds/form/builder_spec.rb
@@ -3,9 +3,10 @@
 require 'rails_helper'
 
 describe Tailwinds::Form::Builder, type: :view do
+  let(:resource)  { build :user }
+  let(:builder) { Tailwinds::Form::Builder.new :user, resource, view, {} }
+
   describe '#text_field' do
-    let(:resource)  { build :user }
-    let(:builder) { Tailwinds::Form::Builder.new :user, resource, view, {} }
     let(:output) do
       builder.text_field :email
     end
@@ -13,6 +14,30 @@ describe Tailwinds::Form::Builder, type: :view do
     it do
       expect(output).to have_selector 'label.block.text-gray-700.text-sm.font-bold.mb-2'
       expect(output).to have_selector 'input.w-full.px-3.py-2.border.border-gray-300.rounded'
+    end
+  end
+
+  describe '#password_field' do
+    let(:output) do
+      builder.password_field :password
+    end
+
+    it do
+      expect(output).to have_selector 'label.block.text-gray-700.text-sm.font-bold.mb-2'
+      expect(output).to have_selector 'input.w-full.px-3.py-2.border.border-gray-300.rounded'
+    end
+  end
+
+  describe '#file_field' do
+    let(:output) do
+      builder.file_field :file
+    end
+
+    it do
+      expect(output).to have_selector 'label.inline-block.bg-blue-500.text-white.font-bold.py-2.px-4.rounded'
+      expect(output).to have_selector(
+        "input[onchange=\"document.getElementById(\'file_label\').textContent = this.files[0].name\"]"
+      )
     end
   end
 end
