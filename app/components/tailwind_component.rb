bbc7af9e7538c263a0040d46956a4151b79903e1
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-01-04 14:48:58 +0400

    Tailwind styled select (#37)
    * Add Ruby-3.3 to CI
    
    * Tailwind Components Refactoring
    
    * Update README
    
    * Refactored default options

--- app/components/tailwind_component.rb
+++ app/components/tailwind_component.rb
@@ -4,4 +4,12 @@ require 'view_component'
 
 # Base TailwindComponent. Contains base features for all tailwind components
 class TailwindComponent < ViewComponent::Base
+  extend Dry::Initializer[undefined: false]
+
+  option :template
+  option :attribute
+  option :object_name
+  option :options
+  option :label
+  option :for
 end
--- spec/components/tailwinds/form/builder_spec.rb
+++ spec/components/tailwinds/form/builder_spec.rb
@@ -49,4 +49,17 @@ describe Tailwinds::Form::Builder, type: :view do
       end
     end
   end
+
+  describe '#select' do
+    let(:output) do
+      builder.select :role, %i[admin user]
+    end
+
+    it do
+      expect(output).to have_selector 'label.block.text-gray-700.text-sm.font-bold.mb-2'
+      expect(output).to have_selector 'select.bg-white.border.border-gray-300.text-gray-700.py-2.px-2.rounded-lg'
+      expect(output).to have_selector 'option[value="admin"]'
+      expect(output).to have_selector 'option[value="user"]'
+    end
+  end
 end
