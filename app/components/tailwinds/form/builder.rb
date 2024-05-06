e957b65de7e387871d9207b3adfb6224f3a8592b
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-05-07 01:42:31 +0400

    Fix select include_blank

--- app/components/tailwinds/form/builder.rb
+++ app/components/tailwinds/form/builder.rb
@@ -30,7 +30,7 @@ module Tailwinds
         render(Tailwinds::Form::SelectComponent.new(
                  input: input(:select),
                  value: options[:selected] || object.public_send(attribute),
-                 collection:,
+                 collection: explicitly_add_blank_option(collection, options),
                  **default_options(attribute, options)
                ), &)
       end
@@ -66,6 +66,18 @@ module Tailwinds
 
       def for_id(attribute)
         "#{object_name}_#{attribute}"
+      end
+
+      # REMOVE IT. WE MUST UNDERSTAND WHY INCLUDE_BLANK DOES NOT WORK
+      # :reek:UtilityFunction
+      def explicitly_add_blank_option(collection, options)
+        if options[:include_blank]
+          collection = collection.to_a if collection.is_a? Hash
+
+          collection.unshift([nil, ''])
+        else
+          collection
+        end
       end
     end
   end
