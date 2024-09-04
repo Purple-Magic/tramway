332900ae003534dcac6fdf4050dcbb612a627942
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-09-04 04:08:21 +0400

    Multiselect. Updates

--- spec/features/multiselect_input_spec.rb
+++ spec/features/multiselect_input_spec.rb
@@ -1,6 +1,11 @@
 feature "MultiselectComponent", type: :feature, js: true do
   scenario "allows user to select multiple options" do
     visit new_user_path
-    binding.pry
+
+    find('#user_role_multiselect').click
+
+    find('.mx-2.leading-6', text: 'Admin').click
+
+    click_on 'Create user'
   end
 end
