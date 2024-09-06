a2bc8bd3a803cf584551700d3840ed476dcb5057
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-09-06 06:44:54 +0400

    Checks

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,6 @@ require File.expand_path('dummy/config/environment', __dir__)
 
 require 'spec_helper'
 require 'rspec/rails'
-require 'webdrivers/chromedriver'
 require 'view_component/test_helpers'
 require 'view_component/system_test_helpers'
 require 'capybara/rspec'
@@ -13,7 +12,7 @@ require 'tramway/helpers/navbar_helper'
 require 'tramway/navbar'
 require 'factory_bot_rails'
 
-Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f } # rubocop:disable Lint/RedundantDirGlobSort
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.include ViewComponent::TestHelpers, type: :component
@@ -25,12 +24,14 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
-Capybara.register_driver :headless_chrome do |app|
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu no-sandbox disable-dev-shm-usage])
-  )
-end
+# require 'webdrivers/chromedriver'
 
-Capybara.javascript_driver = :headless_chrome
+# Capybara.register_driver :headless_chrome do |app|
+#   Capybara::Selenium::Driver.new(
+#     app,
+#     browser: :chrome,
+#     options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu no-sandbox disable-dev-shm-usage])
+#   )
+# end
+
+# Capybara.javascript_driver = :headless_chrome
