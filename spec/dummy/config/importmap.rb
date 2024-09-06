de0e108f8823743aa105ab5408f0cc4e5abb3437
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-09-06 13:47:49 +0400

    Checks 13:47

--- spec/dummy/config/importmap.rb
+++ spec/dummy/config/importmap.rb
@@ -2,4 +2,4 @@
 
 pin 'application'
 pin '@tramway/multiselect', to: 'tramway/multiselect_controller.js'
-pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.2
+pin '@hotwired/stimulus', to: 'hotwired/stimulus.js'
