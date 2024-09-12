7786806490accf0ddb9b8aa0d66860175fb6423f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-09-13 02:25:37 +0400

    Issue #81. Fix routes namespace

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Tramway::Engine, at: '/tramway'
+  mount Tramway::Engine, at: '/admin'
 
   # resources :users
   # resources :clients
