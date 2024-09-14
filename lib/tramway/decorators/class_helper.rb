7c9d2665708d1f9d53db62fe0eeb03da527aa148
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-09-15 03:09:07 +0400

    Issue #81. Implement error message for non-existent decorator class

--- lib/tramway/decorators/class_helper.rb
+++ lib/tramway/decorators/class_helper.rb
@@ -10,14 +10,24 @@ module Tramway
         if decorator.present?
           decorator
         else
-          klass = if Tramway::Decorators::CollectionDecorators.collection?(object_or_array)
-                    object_or_array.first.class
-                  else
-                    object_or_array.class
-                  end
-
-          Tramway::Decorators::NameBuilder.default_decorator_class_name(klass).constantize
+          decorator_class_name(object_or_array).constantize
         end
+      end
+
+      def decorator_class_name(object_or_array)
+        klass = if Tramway::Decorators::CollectionDecorators.collection?(object_or_array)
+                  object_or_array.first.class
+                else
+                  object_or_array.class
+                end
+
+        Tramway::Decorators::NameBuilder.default_decorator_class_name(klass)
+      end
+
+      def decorator_class!(object_or_array, decorator = nil, message:)
+        decorator_class(object_or_array, decorator)
+      rescue NameError
+        raise NameError, "You should define #{decorator_class_name(object_or_array)} decorator class. #{message}"
       end
     end
   end
