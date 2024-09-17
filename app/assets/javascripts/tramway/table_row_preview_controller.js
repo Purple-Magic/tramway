e0fe46c2b72583efd2b42457ca589bd27afdd53e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-09-17 04:07:38 +0400

    Issue #81. Many updates

--- app/assets/javascripts/tramway/table_row_preview_controller.js
+++ app/assets/javascripts/tramway/table_row_preview_controller.js
@@ -3,8 +3,13 @@ import { Controller } from "@hotwired/stimulus";
 export default class TableRowPreview extends Controller {
   static targets = ["main"]
 
+  static values = {
+    cells: Array,
+  }
+
   connect() {
     console.log('connected')
+    this.items = JSON.parse(this.element.dataset.items)
   }
 
   toggle() {
