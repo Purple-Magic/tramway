ddbfddf29102c7e880069da88b18dd00b45e3768
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-09-17 05:01:32 +0400

    Issue #81. Preview is ready

--- app/assets/javascripts/tramway/table_row_preview_controller.js
+++ app/assets/javascripts/tramway/table_row_preview_controller.js
@@ -10,19 +10,26 @@ export default class TableRowPreview extends Controller {
   toggle() {
     const rollUp = document.getElementById("roll-up");
 
-    const titleText = document.createElement("h3");
-    titleText.classList.add("text-xl");
-    titleText.classList.add("dark:text-white");
-    titleText.classList.add("py-4");
-    titleText.classList.add("px-2");
-    titleText.textContent = Object.values(this.items)[0];
 
     const existingTable = rollUp.querySelector(".div-table");
     if (existingTable) {
       existingTable.remove();
     }
 
+    const existingTitle = rollUp.querySelector("h3");
+    if (existingTitle) {
+      existingTitle.remove();
+    }
+
     if (Object.keys(this.items).length === 0) return;
+
+    const titleText = document.createElement("h3");
+
+    titleText.classList.add("text-xl");
+    titleText.classList.add("dark:text-white");
+    titleText.classList.add("py-4");
+    titleText.classList.add("px-2");
+    titleText.textContent = Object.values(this.items)[0];
 
     const table = this.createTable(this.items);
 
@@ -30,6 +37,11 @@ export default class TableRowPreview extends Controller {
     rollUp.insertAdjacentElement('afterbegin', titleText);
 
     rollUp.classList.toggle("hidden");
+  }
+
+  close() {
+    const rollUp = document.getElementById("roll-up");
+    rollUp.classList.add("hidden");
   }
 
   createTable(items) {
@@ -51,11 +63,16 @@ export default class TableRowPreview extends Controller {
     const keyRow = document.createElement("div");
     keyRow.classList.add("div-table-row");
     keyRow.classList.add("dark:bg-gray-700");
+    keyRow.classList.add("px-2");
+    keyRow.classList.add("py-1");
+    keyRow.classList.add("text-xs");
     keyRow.textContent = key;
 
     const valueRow = document.createElement("div"); 
     valueRow.classList.add("div-table-row");
     valueRow.classList.add("dark:bg-gray-800");
+    valueRow.classList.add("px-2");
+    valueRow.classList.add("py-2");
     valueRow.textContent = value;
 
     return [keyRow, valueRow];
