72da1bb047afa31a98f8659340dd0ea01d0ed036
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-09-17 04:50:09 +0400

    Issue #81. Many updates

--- app/assets/javascripts/tramway/table_row_preview_controller.js
+++ app/assets/javascripts/tramway/table_row_preview_controller.js
@@ -1,22 +1,65 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class TableRowPreview extends Controller {
-  static targets = ["main"]
-
-  static values = {
-    cells: Array,
-  }
+  static targets = ["main"];
 
   connect() {
-    console.log('connected')
-    this.items = JSON.parse(this.element.dataset.items)
+    this.items = JSON.parse(this.element.dataset.items || '{}');
   }
 
   toggle() {
-    console.log('work')
-    // this.mainTarget.classList.toggle("hidden");
-    document.getElementById("roll-up").classList.toggle("hidden");
+    const rollUp = document.getElementById("roll-up");
+
+    const titleText = document.createElement("h3");
+    titleText.classList.add("text-xl");
+    titleText.classList.add("dark:text-white");
+    titleText.classList.add("py-4");
+    titleText.classList.add("px-2");
+    titleText.textContent = Object.values(this.items)[0];
+
+    const existingTable = rollUp.querySelector(".div-table");
+    if (existingTable) {
+      existingTable.remove();
+    }
+
+    if (Object.keys(this.items).length === 0) return;
+
+    const table = this.createTable(this.items);
+
+    rollUp.insertAdjacentElement('afterbegin', table);
+    rollUp.insertAdjacentElement('afterbegin', titleText);
+
+    rollUp.classList.toggle("hidden");
+  }
+
+  createTable(items) {
+    const table = document.createElement("div");
+    table.classList.add("div-table");
+    table.classList.add("dark:text-white");
+    table.classList.add("px-2");
+
+    Object.entries(items).forEach(([key, value]) => {
+      const rows = this.createTableRow(key, value);
+
+      rows.forEach((row) => table.appendChild(row));
+    });
+
+    return table;
+  }
+
+  createTableRow(key, value) {
+    const keyRow = document.createElement("div");
+    keyRow.classList.add("div-table-row");
+    keyRow.classList.add("dark:bg-gray-700");
+    keyRow.textContent = key;
+
+    const valueRow = document.createElement("div"); 
+    valueRow.classList.add("div-table-row");
+    valueRow.classList.add("dark:bg-gray-800");
+    valueRow.textContent = value;
+
+    return [keyRow, valueRow];
   }
 }
 
-export { TableRowPreview }
+export { TableRowPreview };
