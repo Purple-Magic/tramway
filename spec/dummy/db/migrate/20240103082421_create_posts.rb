051a704e5866787b0a35b0d7fba999595d421593
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-11-19 04:10:29 +0400

    Test CI

--- spec/dummy/db/migrate/20240103082421_create_posts.rb
+++ spec/dummy/db/migrate/20240103082421_create_posts.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreatePosts < ActiveRecord::Migration[7.1]
+class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.integer :user_id
--- spec/dummy/db/migrate/20240720025030_add_aasm_state_to_posts.rb
+++ spec/dummy/db/migrate/20240720025030_add_aasm_state_to_posts.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddAasmStateToPosts < ActiveRecord::Migration[7.1]
+class AddAasmStateToPosts < ActiveRecord::Migration[7.0]
   def change
     add_column :posts, :aasm_state, :string
   end
