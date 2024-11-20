88e29f66ad7fb21675a64b7078593f2ac97a67fb
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-11-20 04:22:11 +0400

    Issue #81. Add dark mode to all pagination buttons

--- app/helpers/tramway/application_helper.rb
+++ app/helpers/tramway/application_helper.rb
@@ -9,5 +9,9 @@ module Tramway
     def component(name, *, **, &)
       render("#{name}_component".classify.constantize.new(*, **), &)
     end
+
+    def pagination_classes(klass: nil)
+      "cursor-pointer px-3 py-2 font-medium text-purple-700 bg-white rounded-md hover:bg-purple-100 dark:text-white dark:bg-gray-800 dark:hover:bg-gray-700 #{klass}"
+    end
   end
 end
