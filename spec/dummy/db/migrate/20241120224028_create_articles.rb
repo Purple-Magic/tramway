cb2bad7ae4983f35149d91d3f7b47a959da41c01
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-11-21 03:13:59 +0400

    Issue #81. Fix migrations

--- spec/dummy/db/migrate/20241120224028_create_articles.rb
+++ spec/dummy/db/migrate/20241120224028_create_articles.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateArticles < ActiveRecord::Migration[7.2]
+class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
       t.string :title
