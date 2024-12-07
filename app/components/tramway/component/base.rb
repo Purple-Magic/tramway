a91746330c47ec480d2beb7362a5aac06d60cc19
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2024-12-07 18:57:40 +0400

    Issue #81. Create common LabelComponent

--- app/components/tramway/component/base.rb
+++ app/components/tramway/component/base.rb
@@ -5,6 +5,7 @@ module Tramway
     # You can use this class as a base for all your components
     class Base < ViewComponent::Base
       extend Dry::Initializer[undefined: false]
+      include Tramway::Helpers::ComponentHelper
     end
   end
 end
