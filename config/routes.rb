ca568c291e901f7327f918d9c0ffc49e9946c237
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-01-20 22:36:15 +0400

    Prepare routes for namespaced models

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,29 @@
 
 Tramway::Engine.routes.draw do
   Tramway.config.entities.each do |entity|
-    resources entity.name.pluralize.to_sym, only: entity.pages, controller: :entities, defaults: { entity: entity.name }
+    define_resource = lambda do |resource_name, entity| # rubocop:disable Lint/ShadowingOuterLocalVariable
+      resources resource_name.pluralize.to_sym,
+                only: [:index],
+                controller: '/tramway/entities',
+                defaults: { entity: }
+    end
+
+    if entity.name.include?('/')
+      *namespaces, resource_name = entity.name.split('/')
+
+      define_namespace = lambda do |index|
+        namespace namespaces[index].to_sym do
+          if namespaces[index] == namespaces.last
+            define_resource.call(resource_name, entity)
+          else
+            define_namespace.call(namespaces[index + 1])
+          end
+        end
+      end
+
+      define_namespace.call(0)
+    else
+      define_resource.call(entity.name, entity)
+    end
   end
 end
