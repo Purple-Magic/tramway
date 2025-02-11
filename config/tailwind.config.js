e7198b351e52ec2e3f96dc8b57eb71c312d73f35
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-02-11 23:10:24 +0400

    Tailwind 4. Support

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,21 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './public/*.html',
-    './app/components/**/*.html.haml',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      screens: {
-        ...defaultTheme.screens,
-      },
-    },
-  },
   safelist: [
     'div-table',
     'div-table-row',
@@ -25,19 +8,21 @@ module.exports = {
     'text-4xl',
     'font-bold',
     'xl:hidden',
-    'grid-cols-[1fr]',
-    'grid-cols-[1fr,1fr]',
-    'grid-cols-[1fr,1fr,1fr]',
-    'grid-cols-[1fr,1fr,1fr,1fr]',
-    'grid-cols-[1fr,1fr,1fr,1fr,1fr]',
-    'grid-cols-[1fr,1fr,1fr,1fr,1fr,1fr]',
-    'grid-cols-[1fr,1fr,1fr,1fr,1fr,1fr,1fr]',
-    'grid-cols-[1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr]',
-    'grid-cols-[1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr]',
-    'grid-cols-[1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr,1fr]',
+    'grid-cols-1',
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'grid-cols-7',
+    'grid-cols-8',
+    'grid-cols-9',
+    'grid-cols-10',
     'text-right',
     'w-2/3',
     'flex',
+    'bg-purple-700',
+    'px-6',
     // multiselect styles
     'absolute', 'shadow', 'top-100', 'z-40', 'w-full', 'lef-0', 'rounded', 'max-h-select', 'overflow-y-auto', 'cursor-pointer', 'rounded-t', 'border-b', 'hover:bg-teal-100', 'items-center', 'border-transparent', 'border-l-2,', 'relative', 'hover:border-teal-100', 'leading-6', 'bg-transparent', 'appearance-none', 'outline-none', 'h-full' , 'justify-center', 'm-1', 'font-medium', 'rounded-full', 'text-teal-700', 'bg-teal-100', 'border', 'border-teal-300', 'text-xs', 'font-normal', 'leading-none', 'max-w-full', 'flex-initial', 'flex-auto', 'flex-row-reverse',, 'flex-col', 'min-w-96', 'w-fit', 'flex-wrap', 'w-8', 'border-l',
     {
@@ -51,11 +36,5 @@ module.exports = {
     {
       pattern: /(m|p)(l|r|b|t|x|y)-\d+/
     },
-  ],
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
   ],
 };
