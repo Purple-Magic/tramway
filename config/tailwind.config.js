2e8a5055f6ecf0b1fd2699ce6890bef1b9a3dc30
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-02-14 23:11:00 +0400

    Tailwind 4. Update safelist

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -23,22 +23,19 @@ module.exports = {
     'flex',
     'bg-purple-700',
     'px-6',
+    'py-4',
     'dark:placeholder-gray-400',
     'dark:text-white',
     'dark:bg-gray-800',
     'dark:bg-gray-900',
+    'dark:hover:bg-gray-700',
+    'dark:bg-gray-700',
+    'dark:text-gray-400',
+    'dark:hover:bg-gray-700',
+    'dark:hover:text-gray-400',
+    'ml-4',
+    'text-base',
     // multiselect styles
     'absolute', 'shadow', 'top-100', 'z-40', 'w-full', 'lef-0', 'rounded', 'max-h-select', 'overflow-y-auto', 'cursor-pointer', 'rounded-t', 'border-b', 'hover:bg-teal-100', 'items-center', 'border-transparent', 'border-l-2,', 'relative', 'hover:border-teal-100', 'leading-6', 'bg-transparent', 'appearance-none', 'outline-none', 'h-full' , 'justify-center', 'm-1', 'font-medium', 'rounded-full', 'text-teal-700', 'bg-teal-100', 'border', 'border-teal-300', 'text-xs', 'font-normal', 'leading-none', 'max-w-full', 'flex-initial', 'flex-auto', 'flex-row-reverse',, 'flex-col', 'min-w-96', 'w-fit', 'flex-wrap', 'w-8', 'border-l',
-    {
-      pattern: /(text|bg|border)-(purple|blue|gray|yellow|red|white|green)-\d+/,
-      variants: ['dark', 'focus', 'hover', 'dark:hover'],
-    },
-    {
-      pattern: /(text|bg)-(purple|blue|gray|yellow|red|white)/,
-      variants: ['dark', 'focus', 'hover', 'dark:hover'],
-    },
-    {
-      pattern: /(m|p)(l|r|b|t|x|y)-\d+/
-    },
   ],
-};
+}
