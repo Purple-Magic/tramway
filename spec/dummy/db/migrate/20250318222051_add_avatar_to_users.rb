67fd851a4ae5faae0bc39311deba053bfc3aa45d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-03-19 03:44:37 +0400

    Change migration versions

--- spec/dummy/db/migrate/20250318222051_add_avatar_to_users.rb
+++ spec/dummy/db/migrate/20250318222051_add_avatar_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddAvatarToUsers < ActiveRecord::Migration[7.2]
+class AddAvatarToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :avatar, :string
   end
--- spec/dummy/db/migrate/20250318222237_add_info_to_users.rb
+++ spec/dummy/db/migrate/20250318222237_add_info_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddInfoToUsers < ActiveRecord::Migration[7.2]
+class AddInfoToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :country, :string
     add_column :users, :personal_info, :string
