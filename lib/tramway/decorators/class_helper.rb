35020243c317fa50a4ca1fe482f26aeab822e2c3
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-05-02 13:55:30 +0400

    Add namespace to tramway_decorate

--- lib/tramway/decorators/class_helper.rb
+++ lib/tramway/decorators/class_helper.rb
@@ -6,27 +6,29 @@ module Tramway
     module ClassHelper
       module_function
 
-      def decorator_class(object_or_array, decorator = nil)
+      def decorator_class(object_or_array, decorator = nil, namespace = nil)
         raise_error_if_object_empty object_or_array, decorator
 
         return decorator if decorator.present?
 
         begin
-          class_name = decorator_class_name(object_or_array)
+          class_name = decorator_class_name(object_or_array, namespace)
           class_name.constantize
         rescue NameError
           raise NameError, "You should define #{class_name} decorator class."
         end
       end
 
-      def decorator_class_name(object_or_array)
+      def decorator_class_name(object_or_array, namespace)
         klass = if Tramway::Decorators::CollectionDecorators.collection?(object_or_array)
                   object_or_array.first.class
                 else
                   object_or_array.class
                 end
 
-        Tramway::Decorators::NameBuilder.default_decorator_class_name(klass)
+        base_class_name = Tramway::Decorators::NameBuilder.default_decorator_class_name(klass)
+
+        namespace.present? ? "#{namespace}::#{base_class_name}" : base_class_name
       end
 
       # :reek:NilCheck { enabled: false }
