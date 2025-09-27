a9a54209c5a0723fce41c6000a923195877ecb79
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-09-28 00:07:56 +0400

    Add optional table headers

--- app/components/tailwinds/table/header_component.rb
+++ app/components/tailwinds/table/header_component.rb
@@ -4,7 +4,12 @@ module Tailwinds
   module Table
     # Component for rendering a header in a table
     class HeaderComponent < Tramway::Component::Base
-      option :headers
+      option :headers, optional: true, default: -> { [] }
+      option :columns, optional: true, default: -> { 3 }
+
+      def columns_count
+        headers.present? ? headers.size : columns
+      end
     end
   end
 end
