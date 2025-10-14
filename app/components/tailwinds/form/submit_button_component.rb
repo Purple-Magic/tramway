5ac4aa82172f620863f49461b4903390132af392
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-10-14 06:39:31 +0400

    Propagate form size from tramway_form_for (#103)

--- app/components/tailwinds/form/submit_button_component.rb
+++ app/components/tailwinds/form/submit_button_component.rb
@@ -4,10 +4,18 @@ module Tailwinds
   module Form
     # Tailwind-styled submit button
     class SubmitButtonComponent < TailwindComponent
-      def initialize(action, **options)
-        @options = options.except :type
-
+      def initialize(action, size: :middle, **options)
         @text = action.is_a?(String) ? action : action.to_s.capitalize
+
+        super(
+          input: nil,
+          attribute: nil,
+          value: nil,
+          options: options.except(:type),
+          label: nil,
+          for: nil,
+          size:
+        )
       end
     end
   end
