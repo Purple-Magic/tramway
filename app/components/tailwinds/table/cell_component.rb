114e8255a201bd13746415b3e07cbfaa3580e8e3
Egor Lukin <mail@egorlukin.me>
2025-10-21 14:49:50 +0300

    feat: add custom classes option for cells, headers

--- app/components/tailwinds/table/cell_component.rb
+++ app/components/tailwinds/table/cell_component.rb
@@ -4,6 +4,23 @@ module Tailwinds
   module Table
     # Component for rendering a cell in a table
     class CellComponent < Tramway::Component::Base
+      option :options, optional: true, default: -> { {} }
+
+      def cell_tag
+        classes = [*default_classes, options[:class]].join(' ')
+
+        tag.div(**options, class: classes) do
+          yield if block_given?
+        end
+      end
+
+      private
+
+      def default_classes
+        [
+          'div-table-cell', 'px-6', 'py-4', 'font-medium', 'text-gray-900', 'whitespace-nowrap', 'dark:text-white', 'text-xs.sm:text-base'
+        ]
+      end
     end
   end
 end
--- app/components/tailwinds/table/header_component.rb
+++ app/components/tailwinds/table/header_component.rb
@@ -6,9 +6,31 @@ module Tailwinds
     class HeaderComponent < Tramway::Component::Base
       option :headers, optional: true, default: -> { [] }
       option :columns, optional: true, default: -> { 3 }
+      option :options, optional: true, default: -> { {} }
 
       def columns_count
         headers.present? ? headers.size : columns
+      end
+
+      def header_tag
+        default_attributes = { aria: { label: "Table Header" }, role: :row }
+
+        classes = [*default_classes, options[:class]].join(' ')
+
+        tag.div(**options.merge(default_attributes), class: classes) do
+          yield if block_given?
+        end
+      end
+
+      private
+
+      def default_classes
+        [
+          'div-table-row', 'grid', 'text-white',
+          'text-small', 'gap-4', 'bg-purple-700',
+          'dark:bg-gray-700', 'dark:text-gray-400',
+          "grid-cols-#{columns_count}"
+        ]
       end
     end
   end
