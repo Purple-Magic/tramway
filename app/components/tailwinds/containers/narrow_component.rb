1a55fd24d58a9e67d518cc77c55bbfd421c09462
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-10-25 05:46:01 +0400

    iFix

--- app/components/tailwinds/containers/narrow_component.rb
+++ app/components/tailwinds/containers/narrow_component.rb
@@ -3,7 +3,7 @@
 module Tailwinds
   module Containers
     # Default page container in Tramway
-    class NarrowComponent < ::TailwindComponent
+    class NarrowComponent < Tramway::Component::Base
       option :id, optional: true, default: proc { SecureRandom.uuid }
     end
   end
