ba9594d60b485aca363c337f03394baf84bb8250
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-11-24 04:11:11 +0400

    Fix migration version

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 20_251_123_223_821) do
+ActiveRecord::Schema[7.1].define(version: 20_251_123_223_821) do
   create_table 'articles', force: :cascade do |t|
     t.datetime 'created_at', null: false
     t.string 'text'
