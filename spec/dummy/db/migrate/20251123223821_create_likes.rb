6e47409cdeb2da5b6d2e37f1f2c2026d1734cbfc
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-11-24 04:05:39 +0400

    Fix migration

--- spec/dummy/db/migrate/20251123223821_create_likes.rb
+++ spec/dummy/db/migrate/20251123223821_create_likes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateLikes < ActiveRecord::Migration[8.1]
+class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
       t.integer :post_id
