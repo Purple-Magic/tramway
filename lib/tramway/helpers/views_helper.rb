68d3ed4dadea6813a707a1039306c55854f62587
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-11-26 03:12:48 +0400

    Fix tramway_button

--- lib/tramway/helpers/views_helper.rb
+++ lib/tramway/helpers/views_helper.rb
@@ -36,7 +36,7 @@ module Tramway
         component 'tailwinds/table/cell', &
       end
 
-      def tramway_button(path: nil, text: nil, method: :get, link: false, **options)
+      def tramway_button(path: nil, text: nil, method: :get, link: false, **options, &)
         component 'tailwinds/button',
                   text:,
                   path:,
@@ -45,7 +45,8 @@ module Tramway
                   color: options.delete(:color),
                   type: options.delete(:type),
                   size: options.delete(:size),
-                  options:
+                  options:,
+                  &
       end
 
       def tramway_back_button
