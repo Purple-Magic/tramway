4983b7bd287393b99f72a67f3392a37623121e80
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-12-04 17:50:17 +0400

    Fix tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -34,6 +34,16 @@ module.exports = {
     'grid-cols-8',
     'grid-cols-9',
     'grid-cols-10',
+    'md:grid-cols-1',
+    'md:grid-cols-2',
+    'md:grid-cols-3',
+    'md:grid-cols-4',
+    'md:grid-cols-5',
+    'md:grid-cols-6',
+    'md:grid-cols-7',
+    'md:grid-cols-8',
+    'md:grid-cols-9',
+    'md:grid-cols-10',
 
     // === Page container and alignment helpers ===
     'container',
