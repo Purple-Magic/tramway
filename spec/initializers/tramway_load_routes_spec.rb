56d2d2ef683fe6185653b42387e038b66c241081
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-12-07 07:18:31 +0400

    Fix tests

--- spec/initializers/tramway_load_routes_spec.rb
+++ spec/initializers/tramway_load_routes_spec.rb
@@ -4,16 +4,33 @@ require 'rails_helper'
 
 describe 'Tramway::Engine load_routes initializer', type: :routing do
   it 'defines routes for each entity in Tramway::Config.entities' do
+    entity_options = {
+      name: 'post',
+      namespace: :admin,
+      pages: [
+        {
+          action: 'index',
+          scope: :published
+        },
+        {
+          action: 'show'
+        },
+        {
+          action: :create
+        }
+      ]
+    }
+
     expect(get: '/admin/posts').to route_to(
       controller: 'tramway/entities',
       action: 'index',
-      entity: build(:entity, name: 'post', pages: [{ action: 'index', scope: :published }, { action: 'show' }])
+      entity: build(:entity, **entity_options)
     )
 
     expect(get: '/admin/comments').to route_to(
       controller: 'tramway/entities',
       action: 'index',
-      entity: build(:entity, name: 'comment')
+      entity: build(:entity, name: 'comment', namespace: :admin)
     )
   end
 
--- spec/lib/kaminari/helpers/tag_spec.rb
+++ spec/lib/kaminari/helpers/tag_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'kaminari/helpers/tag'
 
 RSpec.describe Kaminari::Helpers::Tag do
   describe '#page_url_for' do
@@ -9,10 +10,10 @@ RSpec.describe Kaminari::Helpers::Tag do
 
     it 'builds path using custom_path_method with passed arguments' do
       tag = described_class.allocate
-      tag.instance_variable_set(:@options, { custom_path_method: :post_path, custom_path_arguments: [post.id] })
+      tag.instance_variable_set(:@options, { custom_path_method: :admin_post_path, custom_path_arguments: [post.id] })
       tag.instance_variable_set(:@params, params)
 
-      expect(tag.page_url_for(2)).to eq(Tramway::Engine.routes.url_helpers.post_path(post.id, page: 2))
+      expect(tag.page_url_for(2)).to eq(Tramway::Engine.routes.url_helpers.admin_post_path(post.id, page: 2))
     end
 
     it 'falls back to default url building when custom_path_method is missing' do
