62a5e481d6108025f9aa38340a3991dc05584aa0
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-12-27 10:27:47 +0400

    Add config for color theme

--- app/components/tailwinds/containers/narrow_component.rb
+++ app/components/tailwinds/containers/narrow_component.rb
@@ -5,6 +5,18 @@ module Tailwinds
     # Default page container in Tramway
     class NarrowComponent < Tramway::BaseComponent
       option :id, optional: true, default: proc { SecureRandom.uuid }
+      option :theme, optional: true, default: -> { :dark }
+
+      def color_classes
+        case theme
+        when :dark
+          'bg-gray-900 text-white'
+        when :light
+          'bg-gray-100 text-gray-900'
+        else
+          'bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-white'
+        end
+      end
     end
   end
 end
