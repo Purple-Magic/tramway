16cc02802f06afc652e653a41c31f61f2b697bce
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2026-01-07 04:05:27 +0400

    Agents.md (#149)
    * Update agents.md
    
    * Linter fixes

--- spec/tramway/generators/install_generator_spec.rb
+++ spec/tramway/generators/install_generator_spec.rb
@@ -7,10 +7,6 @@ require 'generators/tramway/install/install_generator'
 
 RSpec.describe Tramway::Generators::InstallGenerator do
   let(:destination_root) { Dir.mktmpdir }
-  let(:gemfile_path) { File.join(destination_root, 'Gemfile') }
-  let(:tailwind_config_path) { File.join(destination_root, 'config/tailwind.config.js') }
-  let(:tailwind_application_path) { File.join(destination_root, 'app/assets/tailwind/application.css') }
-  let(:template_tailwind_config_path) { File.expand_path('../../../config/tailwind.config.js', __dir__) }
 
   after do
     FileUtils.rm_rf(destination_root)
@@ -22,6 +18,30 @@ RSpec.describe Tramway::Generators::InstallGenerator do
     generator = described_class.new
     generator.destination_root = destination_root
     generator.invoke_all
+  end
+
+  def gemfile_path
+    File.join(destination_root, 'Gemfile')
+  end
+
+  def tailwind_config_path
+    File.join(destination_root, 'config/tailwind.config.js')
+  end
+
+  def tailwind_application_path
+    File.join(destination_root, 'app/assets/tailwind/application.css')
+  end
+
+  def template_tailwind_config_path
+    File.expand_path('../../../config/tailwind.config.js', __dir__)
+  end
+
+  def agents_path
+    File.join(destination_root, 'AGENTS.md')
+  end
+
+  def template_agents_path
+    File.expand_path('../../../docs/AGENTS.md', __dir__)
   end
 
   describe 'ensuring gem dependencies' do
@@ -119,6 +139,33 @@ RSpec.describe Tramway::Generators::InstallGenerator do
 
       run_generator
       second_run = File.read(tailwind_application_path)
+
+      expect(second_run).to eq(first_run)
+    end
+  end
+
+  describe 'AGENTS instructions' do
+    it 'creates AGENTS file from template when missing' do
+      run_generator
+
+      expect(File).to exist(agents_path)
+      expect(File.read(agents_path)).to eq(File.read(template_agents_path))
+    end
+
+    it 'appends Tramway instructions to existing AGENTS file' do
+      File.write(agents_path, '# Existing instructions')
+
+      run_generator
+
+      expect(File.read(agents_path)).to eq("# Existing instructions\n\n#{File.read(template_agents_path)}")
+    end
+
+    it 'is idempotent when instructions already exist' do
+      run_generator
+      first_run = File.read(agents_path)
+
+      run_generator
+      second_run = File.read(agents_path)
 
       expect(second_run).to eq(first_run)
     end
