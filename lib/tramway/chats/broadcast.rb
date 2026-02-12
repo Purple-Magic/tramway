dedaa0eb8fc93679d0070d2d62c36ad77b3c70cf
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2026-02-12 11:10:45 +0400

    Add chat_id argument to tramway_chat_append_message

--- lib/tramway/chats/broadcast.rb
+++ lib/tramway/chats/broadcast.rb
@@ -5,13 +5,13 @@ module Tramway
     module Broadcast
       MESSAGE_TYPES = %i[sent received].freeze
 
-      def tramway_chat_append_message(message_type:, text:, sent_at:)
+      def tramway_chat_append_message(chat_id:, message_type:, text:, sent_at:)
         raise ArgumentError, 'message_type must be :sent or :received' unless MESSAGE_TYPES.include?(message_type.to_sym)
 
         type = message_type
 
         broadcast_append_to(
-          [chat.user.id, 'messages'],
+          [chat_id, 'messages'],
           target: 'messages',
           partial: 'tramway/chats/message',
           locals: { type:, text:, sent_at: }
--- spec/tramway/chats/broadcast_spec.rb
+++ spec/tramway/chats/broadcast_spec.rb
@@ -10,9 +10,6 @@ RSpec.describe Tramway::Chats::Broadcast do
     Class.new do
       include Tramway::Chats::Broadcast
 
-      def chat
-        OpenStruct.new(user: OpenStruct.new(id: 42))
-      end
 
       def broadcast_append_to(*)
         true
@@ -23,7 +20,7 @@ RSpec.describe Tramway::Chats::Broadcast do
   describe '#tramway_chat_append_message' do
     it 'broadcasts a message with expected payload' do
       expect(broadcaster).to receive(:broadcast_append_to).with(
-        [42, 'messages'],
+        ['chat-42', 'messages'],
         target: 'messages',
         partial: 'tramway/chats/message',
         locals: {
@@ -34,6 +31,7 @@ RSpec.describe Tramway::Chats::Broadcast do
       )
 
       broadcaster.tramway_chat_append_message(
+        chat_id: 'chat-42',
         message_type: :sent,
         text: 'Hello',
         sent_at: Time.zone.parse('2026-01-01 10:00:00')
@@ -42,7 +40,7 @@ RSpec.describe Tramway::Chats::Broadcast do
 
     it 'raises when message_type is invalid' do
       expect do
-        broadcaster.tramway_chat_append_message(message_type: :invalid, text: 'Hello', sent_at: Time.current)
+        broadcaster.tramway_chat_append_message(chat_id: 'chat-42', message_type: :invalid, text: 'Hello', sent_at: Time.current)
       end.to raise_error(ArgumentError, 'message_type must be :sent or :received')
     end
   end
