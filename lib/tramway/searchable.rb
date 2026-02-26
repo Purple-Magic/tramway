98cb9c8807b22694b1ba6f98f096696565d45c3d
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2026-02-26 05:57:59 +0400

    Tramway CRUD: upgrade search feature

--- lib/tramway/searchable.rb
+++ lib/tramway/searchable.rb
@@ -8,19 +8,35 @@ module Tramway
 
     class_methods do
       def tramway_search(query)
-        return all if query.blank?
+        tokens = search_tokens(query)
+        columns_to_search = searchable_column_names
 
-        columns_to_search = columns.select { |c| c.type.in?(%i[string text]) }.map(&:name)
-        return all if columns_to_search.empty?
-
-        tsvector = columns_to_search.map do |column|
-          "coalesce(#{connection.quote_column_name(column)}, '')"
-        end.join(" || ' ' || ")
+        return all if tokens.empty? || columns_to_search.empty?
 
         where(
-          Arel.sql("to_tsvector('simple', #{tsvector}) @@ websearch_to_tsquery('simple', ?)"),
-          query
+          Arel.sql("to_tsvector('simple', #{tsvector_expression(columns_to_search)}) @@ to_tsquery('simple', ?)"),
+          tsquery_expression(tokens)
         )
+      end
+
+      private
+
+      def search_tokens(query)
+        query.to_s.scan(/[[:alnum:]]+/)
+      end
+
+      def searchable_column_names
+        columns.select { |column| column.type.in?(%i[string text]) }.map(&:name)
+      end
+
+      def tsvector_expression(column_names)
+        column_names.map do |column_name|
+          "coalesce(#{connection.quote_column_name(column_name)}, '')"
+        end.join(" || ' ' || ")
+      end
+
+      def tsquery_expression(tokens)
+        tokens.map { |token| "#{token}:*" }.join(' & ')
       end
     end
   end
